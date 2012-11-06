27f0bade25dc70492e55ad028fcb45d5cc8f23af
Diego <solollamamepatodonald@gmail.com>
2012-11-06 18:29:28 -0300

    Salto calculado, debe mejorar

--- objeto.lua
+++ objeto.lua
@@ -13,14 +13,14 @@ local obj = {}
 	obj.tipo = 0
 	obj.quad = 0
 	obj.vx = 250
-	obj.vy = 250
+	obj.vy = 500
 	obj.aceX = 100
-	obj.aceY = 100
+	obj.aceY = 1000
 
 	obj.vxReal = obj.vx
 	obj.vyReal = obj.vy
 
-	obj.tiempoSalto = 1
+	obj.tiempoSalto = 0.2
 	obj.saltando = 0
 
 	obj.estado = "normal"
@@ -44,26 +44,34 @@ function obj:update( dt )
 	end
 
 	if obj.estado == "saltando" and obj.lugar == "aire" then
-		local coli, cuad = mapa:colisiona(obj.cuadColi.x, obj.cuadColi.y - obj.vy*dt, obj.cuadColi.w, obj.cuadColi.h)
+		local coli, cuad = mapa:colisiona(obj.cuadColi.x, obj.cuadColi.y - obj.vyReal*dt, obj.cuadColi.w, obj.cuadColi.h)
 		if  not coli and obj.saltando < obj.tiempoSalto then
-			obj.cuadColi.y = obj.cuadColi.y - obj.vy*dt
+			obj.cuadColi.y = obj.cuadColi.y - obj.vyReal*dt
 			obj.saltando = obj.saltando + dt
-		--elseif not coli and obj.saltando > obj.tiempoSalto
-
+		elseif not coli and obj.saltando > obj.tiempoSalto and obj.vyReal >= 0 then
+			obj.cuadColi.y = obj.cuadColi.y - obj.vyReal*dt
+			obj.vyReal = obj.vyReal - obj.aceY * dt
 		else
 			obj.estado = "cayendo"
 			obj.saltando = 0
+			obj.vyReal = 0
 		end
 	end
 
 
 	if obj.estado == "cayendo" and obj.lugar == "aire" then
-		local coli, cuad = mapa:colisiona(obj.cuadColi.x, obj.cuadColi.y + obj.vy*dt, obj.cuadColi.w, obj.cuadColi.h)
+		local coli, cuad = mapa:colisiona(obj.cuadColi.x, obj.cuadColi.y + obj.vyReal*dt, obj.cuadColi.w, obj.cuadColi.h)
 		if  not coli then
-			obj.cuadColi.y = obj.cuadColi.y + obj.vy*dt
+			--if obj.vyReal > obj.vy then
+				--obj.cuadColi.y = obj.cuadColi.y + obj.vyReal*dt
+			--else	
+				obj.cuadColi.y = obj.cuadColi.y + obj.vyReal*dt
+				obj.vyReal = obj.vyReal + obj.aceY * dt
+			--end
 		else
 			obj.lugar = "tierra"
 			obj.estado = "normal"
+			obj.vyReal = obj.vy
 		end
 	end
 
